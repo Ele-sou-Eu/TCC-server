6dd9a4a76b9065a956ba625fb4076147bb085c0d
Leo <leo.cubo3@gmail.com>
2021-11-29 17:01:15 -0300

    Alteração de acordo com novos nomes de colunas

--- src/controllers/disciplinaController.js
+++ src/controllers/disciplinaController.js
@@ -3,9 +3,9 @@ import database from "../database/index.js"
 class DisciplinaController{
     async index(request, response){
         const data = await database.select('disciplina.*').table("matricula")
-            .join('curso', 'curso.codigo_curso', 'matricula.Curso')
-            .join('estrutura_curricular', 'estrutura_curricular.Código_curso', 'curso.codigo_curso')
-            .join('disciplina', 'disciplina.codigo_disciplina', 'estrutura_curricular.Código_disciplina')
+            .join('curso', 'curso.codigo_curso', 'matricula.curso')
+            .join('estrutura_curricular', 'estrutura_curricular.codigo_curso', 'curso.codigo_curso')
+            .join('disciplina', 'disciplina.codigo_disciplina', 'estrutura_curricular.codigo_disciplina')
             .where('matricula.id', request.userID)
         response.json(data)
     }
--- src/controllers/requisitoController.js
+++ src/controllers/requisitoController.js
@@ -2,10 +2,10 @@ import database from "../database/index.js"
 
 class RequisitoController{
     async index(request, response){
-        const data = await database.select('pre_req.*', 'disciplina.Nome').table("matricula")
-        .join('curso', 'curso.codigo_curso', 'matricula.Curso')
-        .join('estrutura_curricular', 'estrutura_curricular.Código_curso', 'curso.codigo_curso')
-        .join('disciplina', 'disciplina.codigo_disciplina', 'estrutura_curricular.Código_disciplina')
+        const data = await database.select('pre_req.*', 'disciplina.nome').table("matricula")
+        .join('curso', 'curso.codigo_curso', 'matricula.curso')
+        .join('estrutura_curricular', 'estrutura_curricular.codigo_curso', 'curso.codigo_curso')
+        .join('disciplina', 'disciplina.codigo_disciplina', 'estrutura_curricular.codigo_disciplina')
         .join('pre_req', 'pre_req.codigo_requisito', 'disciplina.codigo_disciplina')
         .where('matricula.id', request.userID)
         response.json(data)
