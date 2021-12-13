1ac10e32bb9ad498e6540a6eeb810aca2b9c2557
Leo <leo.cubo3@gmail.com>
2021-12-13 09:39:20 -0300

    disciplinaController agora retorna semestre

--- src/controllers/disciplinaController.js
+++ src/controllers/disciplinaController.js
@@ -2,7 +2,7 @@ import database from "../database/index.js"
 
 class DisciplinaController{
     async index(request, response){
-        const data = await database.select('disciplina.*').table("matricula")
+        const data = await database.select('disciplina.*', 'estrutura_curricular.semestre').table("matricula")
             .join('curso', 'curso.codigo_curso', 'matricula.curso')
             .join('estrutura_curricular', 'estrutura_curricular.codigo_curso', 'curso.codigo_curso')
             .join('disciplina', 'disciplina.codigo_disciplina', 'estrutura_curricular.codigo_disciplina')
