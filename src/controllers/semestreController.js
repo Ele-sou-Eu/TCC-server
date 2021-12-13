014e70d5e91fc0eeaeba29e679db0cd9ee7571fe
Leo <leo.cubo3@gmail.com>
2021-12-13 09:38:54 -0300

    Implementação semestreController

--- src/controllers/semestreController.js
+++ src/controllers/semestreController.js
@@ -2,13 +2,10 @@ import database from "../database/index.js"
 
 class SemestreController{
     async index(request, response){
-        const data = await database.select()
-        // const data = await database.select('pre_req.*', 'disciplina.nome').table("matricula")
-        // .join('curso', 'curso.codigo_curso', 'matricula.curso')
-        // .join('estrutura_curricular', 'estrutura_curricular.codigo_curso', 'curso.codigo_curso')
-        // .join('disciplina', 'disciplina.codigo_disciplina', 'estrutura_curricular.codigo_disciplina')
-        // .join('pre_req', 'pre_req.codigo_requisito', 'disciplina.codigo_disciplina')
-        // .where('matricula.id', request.userID)
+        const data = await database.select('estrutura_curricular.semestre').table('estrutura_curricular')
+        .join('curso', 'curso.codigo_curso', 'estrutura_curricular.codigo_curso')
+        .join('matricula', 'matricula.curso', 'curso.codigo_curso')
+        .where('matricula.id', request.userID)
         response.json(data)
     }
 }
